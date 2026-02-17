71541642fb2dcc46fcf3ca2696abffebdd6aedd6
Mounika <mounikapothana9@gmail.com>
2026-02-17 12:15:54 +0530

    updated urls

--- app/urls.py
+++ app/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path('task',views.task_view, name = 'task'),
-    path('delete/<int:id>',views.delete_view, name = 'delete'),
-    path('add',views.add_view,name='add'),
-    path('edit/<int:id>',views.edit_view,name='edit'),
-    path('register',views.register_view,name='register'),
-    path('login',views.login_view,name='login'),
-    path('logout',views.logout_view,name='logout')
+    path('task/',views.task_view, name = 'task'),
+    path('delete/<int:id>/',views.delete_view, name = 'delete'),
+    path('add/',views.add_view,name='add'),
+    path('edit/<int:id>/',views.edit_view,name='edit'),
+    path('register/',views.register_view,name='register'),
+    path('login/',views.login_view,name='login'),
+    path('logout/',views.logout_view,name='logout')
 ]
