54af1e898e8b6628ac0f875261836c0cd5e688e3
Mounika <mounikapothana9@gmail.com>
2026-02-17 12:29:50 +0530

    updated main url

--- app/urls.py
+++ app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('delete/<int:id>/',views.delete_view, name = 'delete'),
     path('add/',views.add_view,name='add'),
     path('edit/<int:id>/',views.edit_view,name='edit'),
-    path('register/',views.register_view,name='register'),
+    path('',views.register_view,name='register'),
     path('login/',views.login_view,name='login'),
     path('logout/',views.logout_view,name='logout')
 ]
